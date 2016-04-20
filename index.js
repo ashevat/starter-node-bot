e9557ccf810028f2e0820f58969d2e0ec336b53f
Amir Shevat <amir.shevat@gmail.com>
2016-04-20 15:06:32 -0700

    url encode

--- index.js
+++ index.js
@@ -67,10 +67,10 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   word  = message.text
-  //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+  safe_word = encodeURI(word)
   var options = {
     host: 'www.dictionaryapi.com',
-    path: '/api/v1/references/thesaurus/xml/'+word+'?key=d08999a5-7466-4eca-8051-1b2dfd324740'
+    path: '/api/v1/references/thesaurus/xml/'+safe_word+'?key=d08999a5-7466-4eca-8051-1b2dfd324740'
   };
 
   callback = function(response) {
